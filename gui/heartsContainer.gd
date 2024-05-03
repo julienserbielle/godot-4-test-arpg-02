4bcb305b3690666a8f98d0a0c92e25b3822da982
julienserbielle <julien.serbielle@oclock.school>
2024-05-03 22:00:46 +0200

    Ajout : affichage des sprites coeurs en dynamique

--- gui/heartsContainer.gd
+++ gui/heartsContainer.gd
@@ -1,6 +1,6 @@
 extends HBoxContainer
 
-
+@onready var heartGUIClass = preload("res://gui/heartGui.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func setMaxHearts (max: int): 
+	for i in range(max):
+		var heart = heartGUIClass.instantiate()
+		add_child(heart)
